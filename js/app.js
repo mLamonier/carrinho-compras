c156766437093507e8467d5035c5dc309090111e
unknown <miguel2002lamonier@gmail.com>
2025-03-18 15:54:12 -0300

    Update projeto

--- js/app.js
+++ js/app.js
@@ -1,13 +1,43 @@
+let totalGeral;
+limpar();
+
 function adicionar(){
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0].trim();
     let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade').value;
+
+    if (quantidade < 0) {
+        alert('ERRO: A quantidade não pode ser negativa!');
+        return;
+    }
+    if (quantidade == 0) {
+        alert('ERRO: A quantidade não pode ser 0!');
+        return;
+    }
+
+    if (quantidade > 100) {
+        alert('ERRO: Limite máximo de 100 produtos por pedido!');
+        return;
+    }
+
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `
+        <section class="carrinho__produtos__produto">
+            <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorUnitario}</span>
+        </section>
+    `;
+    
     let preco = quantidade * valorUnitario;
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
 
-    document.getElementById('lista-produtos').innerHTML = `<span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorUnitario}</span>`
-
+    quantidade = document.getElementById('quantidade').value = 0;
 }
 function limpar(){
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0'
+    quantidade = document.getElementById('quantidade').value = 0;
 }
